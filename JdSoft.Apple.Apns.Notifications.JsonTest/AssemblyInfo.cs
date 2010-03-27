dc5a8a90e408c9c610a89b5a9828626c8629014d
redth <jondick@gmail.com>
2010-03-27 18:57:48 +0000

    Updated Version info to 1.0.3.0
    git-svn-id: https://apns-sharp.googlecode.com/svn/trunk@22 eb9801aa-713d-11de-9a45-5127b2b2b1b8

--- JdSoft.Apple.Apns.Notifications.JsonTest/AssemblyInfo.cs
+++ JdSoft.Apple.Apns.Notifications.JsonTest/AssemblyInfo.cs
@@ -17,11 +17,11 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
 
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
-[assembly: AssemblyFileVersionAttribute("1.0.2.0")]
+[assembly: AssemblyFileVersionAttribute("1.0.3.0")]
